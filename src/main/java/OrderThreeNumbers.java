cd5e0b61c1661297e45fadd64772fe1a7b48a25a
Emmanuel Valverde Ramos <evrtrabajo@gmial.com>
2021-10-18 07:50:50 +0200

    basic test suit

--- src/main/java/OrderThreeNumbers.java
+++ src/main/java/OrderThreeNumbers.java
@@ -1,8 +1,29 @@
 public class OrderThreeNumbers {
-    public static void main(String[] args) {
-    }
+    /*public static void main(String[] args) {
+    }*/
 
     public static String orderNumbers(int first,int second, int third) {
+        int aux = 0;
+
+        if (second > third) {
+            aux = second;
+            second = third;
+            third = aux;
+        }
+
+        if (first > second) {
+            aux = first;
+            first = second;
+            second = aux;
+        }
+
+        if (second > third) {
+            aux = third;
+            third = second;
+            second = aux;
+        }
+
+
         return String.format("%d,%d,%d", first, second, third);
     }
 }
--- src/test/java/OrderThreeNumbersTest.java
+++ src/test/java/OrderThreeNumbersTest.java
@@ -7,4 +7,32 @@ public class OrderThreeNumbersTest {
         int first = 0, second = 0, third = 0;
         assertEquals("0,0,0", OrderThreeNumbers.orderNumbers(first,second, third));
     }
+
+    @Test
+    public void should_leave_first_number_if_its_ordinated() {
+        int first = 3, second = 10, third = 10;
+
+        assertEquals("3,10,10", OrderThreeNumbers.orderNumbers(first,second, third));
+    }
+
+    @Test
+    public void should_ordinated_second_numbers_as_first() {
+        int first = 10, second = 3, third = 10;
+
+        assertEquals("3,10,10", OrderThreeNumbers.orderNumbers(first,second, third));
+    }
+
+    @Test
+    public void should_ordinated_last_number_as_first() {
+        int first = 10, second = 10, third = 3;
+
+        assertEquals("3,10,10", OrderThreeNumbers.orderNumbers(first,second, third));
+    }
+
+    @Test
+    public void should_ordinate_big_smallest_and_middle_number() {
+        int first = 7, second = 1, third = 5;
+
+        assertEquals("1,5,7", OrderThreeNumbers.orderNumbers(first,second, third));
+    }
 }
